9c97c7c3ef2ff65332fef0e1573690bf04b80df3
q <q>
2024-12-05 11:33:20 +0100

    zadanie

---
+++ Aplikacje desktopowe i mobilne/ReservationTables/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ReservationTables
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Aplikacje desktopowe i mobilne/ReservationTables/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ReservationTables
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
