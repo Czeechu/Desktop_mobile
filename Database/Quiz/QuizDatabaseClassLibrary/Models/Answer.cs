5780baecbaa1f76c49a03ba3a24e89a8f4284e90
q <q>
2025-02-19 16:00:14 +0100

    zadanie

---
+++ Database/Quiz/QuizDatabaseClassLibrary/Models/Answer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace QuizDatabaseClassLibrary.Models;
+
+[Table("answers")]
+[Index("QuestionId", Name = "fk_question")]
+public partial class Answer
+{
+    [Key]
+    [Column("id", TypeName = "int(11)")]
+    public int Id { get; set; }
+
+    [Column("question_id", TypeName = "int(11)")]
+    public int? QuestionId { get; set; }
+
+    [Column("content", TypeName = "text")]
+    public string Content { get; set; } = null!;
+
+    [Column("isCorrect")]
+    public bool IsCorrect { get; set; }
+
+    [ForeignKey("QuestionId")]
+    [InverseProperty("Answers")]
+    public virtual Question? Question { get; set; }
+}
---
+++ Database/Quiz/QuizDatabaseClassLibrary/Models/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace QuizDatabaseClassLibrary.Models;
+
+[Table("questions")]
+public partial class Question
+{
+    [Key]
+    [Column("id", TypeName = "int(11)")]
+    public int Id { get; set; }
+
+    [Column("content", TypeName = "text")]
+    public string Content { get; set; } = null!;
+
+    [InverseProperty("Question")]
+    public virtual ICollection<Answer> Answers { get; set; } = new List<Answer>();
+}
